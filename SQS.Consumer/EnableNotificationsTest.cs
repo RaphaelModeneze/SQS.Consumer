190de23ec9ab8712ed9bd3eae2acb212882f7f55
RaphaelModeneze <raphaelmodeneze@hotmail.com>
2022-11-13 17:36:42 -0300

    first commit

---
+++ SQS.Consumer/EnableNotificationsTest.cs
@@ -0,0 +1,48 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace SQS.Consumer
+{
+    public class EnableNotificationsTest
+    {
+        private const string bucketName = "rm-dotnet-bucket";
+        private const string sqsQueue = "arn:aws:sqs:sa-east-1:493103478301:NotificationS3";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.SAEast1;
+        private static IAmazonS3 client;
+
+        public static void Main1()
+        {
+            client = new AmazonS3Client(bucketRegion);
+            EnableNotificationAsync().Wait();
+        }
+
+        static async Task EnableNotificationAsync()
+        {
+            try
+            {
+                PutBucketNotificationRequest request = new PutBucketNotificationRequest
+                {
+                    BucketName = bucketName
+                };
+
+                request.QueueConfigurations = new List<QueueConfiguration>();
+                request.QueueConfigurations.Add(new QueueConfiguration()
+                {
+                    Events = new List<EventType> { EventType.ObjectCreatedPut },
+                    Queue = sqsQueue
+                });
+
+                PutBucketNotificationResponse response = await client.PutBucketNotificationAsync(request);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' ", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown error encountered on server. Message:'{0}' ", e.Message);
+            }
+        }
+    }
+}
---
+++ SQS.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+
+namespace SQS.Consumer
+{
+    public class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("SQS.Consumer!");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/493103478301/My-Stack-StandardQueue-ErjE3wksE1AG"
+            };
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    Console.WriteLine(mensagem.Body);
+                    await client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/493103478301/My-Stack-StandardQueue-ErjE3wksE1AG", mensagem.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
